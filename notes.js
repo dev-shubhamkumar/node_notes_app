34a701db86f467026f14498811415e3d844469e7
Shubham Kumar <developer.shubham07@gmail.com>
2022-01-27 13:46:26 +0530

    Initial Code

---
+++ notes.js
@@ -0,0 +1,65 @@
+const chalk = require('chalk')
+const fs = require('fs')
+
+
+const getNotes = () => {
+    return 'Your Notes....'
+}
+
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+
+    const duplicateNotes = notes.filter((note) => note.title === title)
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        console.log(chalk.bgGreen('New Note saved!!!'))
+    } else {
+        console.log(chalk.bgRed('Note title already exists!!! \nPlease try another title. '))
+    }
+
+    saveNotes(notes)
+}
+
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const JSONdata = dataBuffer.toString()
+        return JSON.parse(JSONdata)
+    } catch (e) {
+        return []
+    }
+}
+
+
+const removeNotes = (title) => {
+    const notes = loadNotes()
+
+    const notesToKeep = notes.filter((note) => note.title !== title)
+
+    if (notes.length === notesToKeep.length) {
+        console.log(chalk.bgRed('Failed to remove Note!!! \nPlease provide correct title.'))
+    } else {
+        saveNotes(notesToKeep)
+        console.log(chalk.bgGreen('Note Removed!!!'))
+    }
+}
+
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNotes: removeNotes
+}
